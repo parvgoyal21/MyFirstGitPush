16c11cf58c26716294339458041fb664a4b053ac
unknown <Administrator@WRK134L308BWZ1.bemisco.net>
2020-09-14 16:55:22 +0530

    Intial built

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Weather from './Component/Weather';
+
+function App() {
+  return (
+    <div className="App">
+      <Weather></Weather>
+    </div>
+  );
+}
+
+export default App;
